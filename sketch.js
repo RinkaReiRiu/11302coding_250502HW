73c8a855826cb88e36c3da0f17c34fd4ff243348
Rinka ReiRiu <birdyu1227@gmail.com>
2025-05-02 11:06:02 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 let capture;
+let gridSize = 20; // Size of each square
 
 function setup() {
   createCanvas(windowWidth, windowHeight); // Full window canvas
@@ -10,15 +11,27 @@ function setup() {
 function draw() {
   background('#170033'); // Refresh background each frame
 
-  // Position camera feed at the center and update continuously
   let x = (width - capture.width) / 2;
   let y = (height - capture.height) / 2;
 
   push(); // Start a new drawing state
-    translate(x + capture.width, y); // Move to the right edge of the image
-    scale(-1, 1); // Flip horizontally
-    image(capture, 0, 0, capture.width, capture.height); // Draw flipped image
-  pop(); // Restore the previous drawing state
+  translate(x + capture.width, y); // Move to the right edge of the image
+  scale(-1, 1); // Flip horizontally
+
+  // Loop through grid
+  for (let i = 0; i < capture.width-gridSize; i += gridSize) {
+    for (let j = 0; j < capture.height-gridSize; j += gridSize) {
+      let section = capture.get(i, j, gridSize, gridSize); // Extract part of the image
+      rectMode(CORNER); // Ensure rectangles start at (x,y)
+      fill(255);
+      stroke(0);
+      strokeWeight(2);
+      rect(i, j, gridSize, gridSize, 15); // Draw with rounded corners
+      image(section, i, j, gridSize, gridSize); // Display extracted part
+    }
+  }
+
+  pop(); // Restore previous drawing state
 }
 
 function windowResized() {
